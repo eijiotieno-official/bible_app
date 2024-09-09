79c07b54a332802aea89834e053825d7d7bb2185
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-09-09 13:31:16 +0300

    vb

--- lib/src/services/privacy_policy.dart
+++ lib/src/services/privacy_policy.dart
@@ -1,11 +1,15 @@
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> privacyPolicy() async {
-  final url = "";
+  const url =
+      "https://github.com/eijiotieno-official/bible-app-privacy-policy/blob/main/privacy_policy";
 
   final Uri uri = Uri.parse(url);
   if (await canLaunchUrl(uri)) {
-    await launchUrl(uri);
+    await launchUrl(
+      uri,
+      mode: LaunchMode.inAppWebView,
+    );
   } else {
     throw 'Could not launch $url';
   }
